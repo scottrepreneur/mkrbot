4e8c6a3a9f109cdfcbf02251f43a96061ab93994
scottrepreneur <me@stwoh.com>
2020-02-25 21:54:35 -0600

    clean up spells

--- celery-queue/mkrgov.py
+++ celery-queue/mkrgov.py
@@ -1,5 +1,7 @@
+import json
 import requests
 
+gov_cms_api = 'https://cms-gov.makerfoundation.com/content/governance-dashboard?network=mainnet'
 subgraph_api = 'https://api.thegraph.com/subgraphs/name/protofire/makerdao-governance-v1_5'
 spells_query = """
 {
@@ -16,6 +18,13 @@ spells_query = """
     }
 }
 """
+mkrgov_domain = "https://mkrgov.science/"
+archived_spell_titles = [
+    {
+        "id": "0x8e5f3abc36da63142275202454c11237f47dd170",
+        "title": "Activate the Savings Rate Spread and the Sai and Dai Stability Fee Adjustments."
+    }
+]
 
 def get_spells():
     response = requests.post(subgraph_api, json={'query': spells_query})
@@ -25,6 +34,8 @@ def get_spells():
             spell['hat'] = False
             spell['active'] = False
             spell['prev_cast'] = False
+            spell['link'] = mkrgov_domain + 'executive/' + spell['id']
+            spell['title'] = spell['id']
         return spells
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, spells_query))
@@ -34,7 +45,7 @@ def all_spells():
     all_spells = []
     for spell in spells:
         if float(spell['approvals']) > 50:
-            spell['approvals'] = str(round(float(spell['approvals']), 2))
+            spell['approvals'] = str(round(float(spell['approvals']), 2)).rjust(10, ' ')
             all_spells.append(spell)
     
     return all_spells
@@ -87,19 +98,42 @@ def get_active_spells():
             active_spells.append(spell)
         
     return active_spells
+    
+def get_spell_titles(spells):
+    gov_cms_data = requests.get(gov_cms_api).json()
+
+    for vote in gov_cms_data:
+        for proposal in vote['proposals']:
+            for spell in spells:
+                if proposal['source'].casefold() == spell['id'].casefold():
+                    spell['title'] = proposal['title']
+
+    for title in archived_spell_titles:
+        for spell in spells:
+            if title['id'].casefold() == spell['id'].casefold() and spell['title'] == spell['id']:
+                spell['title'] = title['title']
+
+    return spells
 
 def print_spells():
-    spells = set_hat(previous_spell(all_spells()))
+    spells = get_spell_titles(set_hat(previous_spell(all_spells())))
+    spells.sort(key=lambda x: float(x['approvals']), reverse=True)
+
+    trim_title = 70
 
     message = "" # "Spell count at block _{}_\n".format('blockID')
     for spell in spells:
         if spell['hat']:
-            message = message + "> :tophat: {} Count: {}\n".format(spell['id'], spell['approvals'])
+            message = message + "> {} | [{}]({}) :tophat:\n".format(spell['approvals'], spell['title'][:trim_title], spell['link'])
         elif spell['prev_cast']:
-            message = message + "> last: {} Count: {}\n".format(spell['id'], spell['approvals'])
+            message = message + "> {} | [{}]({}) :back:\n".format(spell['approvals'], spell['title'][:trim_title], spell['link'])
         elif spell ['active']:
-            message = message + "> active: {} Count: {}\n".format(spell['id'], spell['approvals'])
+            message = message + "> {} | [{}]({}) :up:\n".format(spell['approvals'], spell['title'][:trim_title], spell['link'])
         else:
-            message = message + "> {} Count: {}\n".format(spell['id'], spell['approvals'])
+            message = message + "> {} | [{}]({})\n".format(spell['approvals'], spell['title'], spell['link'])
+
+    message = message + "[Executive Votes on mkrgov.science]({})".format(mkrgov_domain + 'executive')
 
     return message
+
+print(print_spells())
